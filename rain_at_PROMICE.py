1bfa4fc3bbcd5cf6cdc178cc6f0fb1c84104d505
Adrien Wehrle <adrien.wehrle@hotmail.fr>
2021-03-14 16:11:24 +0100

    add AW paths

--- rain_at_PROMICE.py
+++ rain_at_PROMICE.py
@@ -16,10 +16,13 @@ from scipy.stats import gaussian_kde
 import os
 import xarray as xr
 
-base_path='/Users/jason/Dropbox/CARRA/CARRA_rain/'
+base_path = '/Users/jason/Dropbox/CARRA/CARRA_rain/'
 
-AW=1
-if AW:base_path='C:/Users/Pascal/Desktop/GEUS_2019/SICE_AW_JEB/SICE_AW_JEB/CARRA_rain/'
+AW = 1
+if AW:
+    base_path = 'C:/Users/Pascal/Desktop/GEUS_2019/SICE_AW_JEB/SICE_AW_JEB/'\
+    + 'CARRA_rain/'
+
 os.chdir(base_path)
 
 # --------------------------- function for distance on sphere
@@ -40,39 +43,46 @@ def haversine_np(lon1, lat1, lon2, lat2):
 
     c = 2 * np.arcsin(np.sqrt(a))
     km = 6367 * c
+    
     return km
 
 # CARRA grid dims
-ni=1269 ; nj=1069
+ni = 1269 
+nj = 1069
 
 # read lat lon arrays
-fn='./ancil/lat_1269x1069.numpy.bin'
-lat=np.fromfile(fn, dtype=float, count=-1, sep='', offset=0)
+fn = './ancil/lat_1269x1069.numpy.bin'
+lat = np.fromfile(fn, dtype=float, count=-1, sep='', offset=0)
 
-fn='./ancil/lon_1269x1069.numpy.bin'
-lon=np.fromfile(fn, dtype=float, count=-1, sep='', offset=0)
+fn = './ancil/lon_1269x1069.numpy.bin'
+lon = np.fromfile(fn, dtype=float, count=-1, sep='', offset=0)
 
 # read PROMICE locations
-meta=pd.read_csv('./ancil/PROMICE_info_w_header_2017-2018_stats.csv',delim_whitespace=True)
+meta = pd.read_csv('./ancil/PROMICE_info_w_header_2017-2018_stats.csv', 
+                   delim_whitespace=True)
 # print(meta.columns)
 
-n=len(meta)
+n = len(meta)
 
 # locate CARRA grid cells closest to PROMICE AWS
-meta["address"]=np.nan
+meta["address"] = np.nan
+
 for i in range(n):
-    dist=haversine_np(meta.lon[i], meta.lat[i], lon, lat)
-    minx=np.min(dist)
-    temp=dist.flatten()
-    v=np.where(temp==minx)
-    meta["address"][i]=v[0]
-    print(meta.name[i],meta.lon[i], meta.lat[i],minx,v[0])
+    
+    dist = haversine_np(meta.lon[i], meta.lat[i], lon, lat)
+    minx = np.min(dist)
+    temp = dist.flatten()
+    v = np.where(temp == minx)
+    meta["address"][i] = v[0]
+    print(meta.name[i], meta.lon[i], meta.lat[i], minx, v[0])
     
 #%% read ~500 Mb CARRA infile
 
-if not AW: ds = xr.open_dataset('/Users/jason/0_dat/CARRA/output/rf_2017.nc')
+if not AW: 
+    ds = xr.open_dataset('/Users/jason/0_dat/CARRA/output/rf_2017.nc')
 
-if AW: ds = xr.open_dataset('H:/CARRA/rf_2016.nc')
+if AW: 
+    ds = xr.open_dataset('H:/CARRA/rf_2016.nc')
 print(ds)
 
 rf = np.array(ds['rf'])
@@ -83,19 +93,20 @@ print(rf.shape)
 # [::-1] flips the array
 
 # test date 220
-temp=rf[220,:,:][::-1].flatten()
+temp = rf[220,:,:][::-1].flatten()
 
 for i in range(n):
-    # lat=meta.lat[i]
-    # lon=meta.lon[i]
-    print(meta.name[i],meta.address[i])
-    point=int(meta.address[i])
+    
+    # lat = meta.lat[i]
+    # lon = meta.lon[i]
+    print(meta.name[i], meta.address[i])
+    point = int(meta.address[i])
     # point=[]
     # makes a line around PROMICE stations otherw2ise difficult to see, so high res!
-    temp[(int(meta.address[i])-20):(int(meta.address[i])+20)]=35
+    temp[(int(meta.address[i]) - 20):(int(meta.address[i]) + 20)] = 35
     # print(meta.name[i],meta.lon[i], meta.lat[i],minx,v[0])
 
-temp=temp.reshape(ni,nj)    
+temp = temp.reshape(ni, nj)    
 
 plt.imshow(temp)
 plt.colorbar()

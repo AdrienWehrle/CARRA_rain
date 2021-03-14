60fd342ad55baee2110d7dfcfd3fadfc7dea0e95
Adrien Wehrle <adrien.wehrle@hotmail.fr>
2021-03-14 17:25:56 +0100

    working on PROMICE extraction

--- extract_PROMICE_data.py
+++ extract_PROMICE_data.py
@@ -14,6 +14,8 @@ import os
 import xarray as xr
 import glob
 import geopandas as gpd
+from scipy.spatial import distance
+from shapely.geometry import Point
 
 base_path = '/Users/jason/Dropbox/CARRA/CARRA_rain/'
 
@@ -41,21 +43,25 @@ lon_mat = lat.reshape(ni, nj)
 meta = pd.read_csv('./ancil/PROMICE_info_w_header_2017-2018_stats.csv', 
                    delim_whitespace=True)
 
+# having a column named "name" create conflicts in DataFrame
+meta.rename(columns={'name': 'station_name'}, inplace=True)
+
+
 # %% point extraction
 
-def match(station_point, era_points):
+def match(station_point, CARRA_points):
     
-    dists = distance.cdist(station_point, era_points)
+    dists = distance.cdist(station_point, CARRA_points)
     
     dist = np.nanmin(dists)
     
     idx = dists.argmin()
     
     station_gdfpoint = Point(station_point[0, 0], station_point[0, 1])
-    matching_era_cell = era_gdfpoints.loc[idx]['geometry']
+    matching_CARRA_cell = CARRA_gdfpoints.loc[idx]['geometry']
     
     res = gpd.GeoDataFrame({'promice_station': [station_gdfpoint], 
-                            'carra_cell': [matching_era_cell],
+                            'CARRA_cell': [matching_CARRA_cell],
                             'distance': pd.Series(dist)})
     
     return res, idx
@@ -63,17 +69,33 @@ def match(station_point, era_points):
 rows, cols = np.meshgrid(np.arange(np.shape(lat_mat)[1]), 
                          np.arange(np.shape(lat_mat)[0]))
 
-carra_positions = pd.DataFrame({'row': rows.ravel(),
+CARRA_positions = pd.DataFrame({'row': rows.ravel(),
                               'col': cols.ravel(),
                               'lon': lon_mat.ravel(),
                               'lat': lat_mat.ravel()})
 
-carra_points = np.vstack((era_positions.lon.ravel(), 
-                          era_positions.lat.ravel())).T
+CARRA_points = np.vstack((CARRA_positions.lon.ravel(), 
+                          CARRA_positions.lat.ravel())).T
 
-carra_gdfpoints = gpd.GeoDataFrame(geometry=gpd.points_from_xy(carra_positions.lon, 
-                                                               carra_positions.lat))
+CARRA_gdfpoints = gpd.GeoDataFrame(geometry=gpd.points_from_xy(CARRA_positions.lon, 
+                                                               CARRA_positions.lat))
 
+# %% match PROMICE locations with CARRA cells 
+
+CARRA_rows = []
+CARRA_cols = []
+for r, station in meta.iterrows():
+    
+    station_point = np.vstack((station.lon,
+                                station.lat)).T
+
+    # get CARRA cell matching station location
+    CARRA_matching_cell, idx = match(station_point, CARRA_points)
+    CARRA_matching_rowcol = CARRA_positions.iloc[idx]
+    
+    
+        
+        
 # %% time series at PROMICE locations
 
 ds = xr.open_dataset('H:/CARRA/rf_2016.nc')
@@ -88,3 +110,11 @@ CARRA_files = glob.glob(CARRA_path + 'rf_*.nc')
 for CARRA_file in CARRA_files:
     
     ds = xr.open_dataset(CARRA_file)
+    
+    annual_results = pd.DataFrame()
+    
+    
+        
+        
+        
+        
